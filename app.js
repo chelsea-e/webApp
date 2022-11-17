97eeb300f7d34ada845553904bf12251ceb9a24e
Chelsea Ehimare <chelseaehimare01@gmail.com>
2022-11-17 02:37:18 +0000

    app.js update

--- app.js
+++ app.js
@@ -3,13 +3,49 @@ const app = new Vue({
     data: {
       page: "content",
       lessons: lessons,
+      sorting:  "price",
+      cart: [],
+      search: ''
     },
     methods: {
+      addtocart(lesson){
+        this.cart.push(lesson)
+      },
+      removefromcart(id){
+        const index = this.cart.findIndex((item) => item.id === id);
+        this.cart.splice(index, 1)
+        if(this.cart.length == 0) {
+          this.page = 'content'
+        }
+      },
+      canaddtocart(lesson){
+        return lesson.spaces > this.cartSpace(lesson)
+      },
+      cartSpace(lesson){
+        let x = 0;
+        for(var i = 0; i < this.cart.length; i++){
+          if(this.cart[i] == lesson){
+            x++;
+          }
+        }
+        return x;
+      },
       navigateTo(page) {
         this.page = page;
       }
     },
     computed:{
-        
+      cartItems: function(){
+        return this.cart.length || "";
+      },
+      filteredLessons: function(){
+        if(this.search){
+        return this.lessons.filter((lesson) => {
+          return lesson.subject.toLowerCase().match(this.search.toLowerCase()) || lesson.location.toLowerCase().match(this.search.toLowerCase())
+        });
+        } 
+
+        return this.lessons
+      }
     },
 });
