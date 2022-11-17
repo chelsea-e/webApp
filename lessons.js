0fd523f965a609fff675bc2e64fbf1022d597546
Chelsea Ehimare <chelseaehimare01@gmail.com>
2022-11-17 01:41:05 +0000

    lessons.js created

---
+++ lessons.js
@@ -0,0 +1,82 @@
+var lessons = [
+    {
+        id: 1,
+        subject: "Mathematics",
+        location: "Houston",
+        price: 85,
+        spaces: 5,
+        images: "img/1.jpg"
+    },
+    {
+        id: 2,
+        subject: "English",
+        location: "Scotland",
+        price: 115,
+        spaces: 5,
+        images: "img/2.jpg"
+    },
+    {
+        id: 3,
+        subject: "Music",
+        location: "Winnipeg",
+        price: 45,
+        spaces: 5,
+        images: "img/3.png"
+    },
+    {
+        id: 4,
+        subject: "Art & Crafts",
+        location: "Paris",
+        price: 90,
+        spaces: 5,
+        images: "img/4.jpeg"
+    },
+    {
+        id: 5,
+        subject: "Nutritional Science",
+        location: "Italy",
+        price: 35,
+        spaces: 5,
+        images: "img/5.png"
+    },
+    {
+        id: 6,
+        subject: "Chemistry",
+        location: "Washington DC",
+        price: 65,
+        spaces: 5,
+        images: "img/6.jpg"
+    },
+    {
+        id: 7,
+        subject: "History",
+        location: "Germany",
+        price: 15,
+        spaces: 5,
+        images: "img/7.png"
+    },
+    {
+        id: 8,
+        subject: "Accounting",
+        location: "New York",
+        price: 100,
+        spaces: 5,
+        images: "img/8.jpg"
+    },
+    {
+        id: 9,
+        subject: "Literature",
+        location: "Australia",
+        price: 90,
+        spaces: 5,
+        images: "img/9.jpg"
+    },
+    {
+        id: 10,
+        subject: "Biology",
+        location: "Japan",
+        price: 35,
+        spaces: 5,
+        images: "img/10.jpg"
+    },
+]
