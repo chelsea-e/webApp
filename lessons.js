0bd9db9fbdcf299ad237b9ea9b8a29ca79520d4d
Chelsea Ehimare <chelseaehimare01@gmail.com>
2022-11-18 08:05:54 +0000

    lesson.js updated

--- lessons.js
+++ lessons.js
@@ -1,82 +1,92 @@
-var lessons = [
+var lessons =  [
     {
-        id: 1,
-        subject: "Mathematics",
-        location: "Houston",
-        price: 85,
-        spaces: 5,
-        images: "img/1.jpg"
+      id: 1,
+      subject: "Mathematics",
+      location: "Houston",
+      price: 85,
+      spaces: 5,
+      icon: "fas fa-calculator",
+      images: "img/1.jpg"
     },
     {
-        id: 2,
-        subject: "English",
-        location: "Scotland",
-        price: 115,
-        spaces: 5,
-        images: "img/2.jpg"
+      id: 2,
+      subject: "English",
+      location: "Scotland",
+      price: 115,
+      spaces: 5,
+      icon: "fas fa-sort-alpha-down",
+      images: "img/2.jpg"
     },
     {
-        id: 3,
-        subject: "Music",
-        location: "Winnipeg",
-        price: 45,
-        spaces: 5,
-        images: "img/3.png"
+      id: 3,
+      subject: "Music",
+      location: "Winnipeg",
+      price: 45,
+      spaces: 5,
+      icon: "fas fa-guitar",
+      images: "img/3.png"
     },
     {
-        id: 4,
-        subject: "Art & Crafts",
-        location: "Paris",
-        price: 90,
-        spaces: 5,
-        images: "img/4.jpeg"
+      id: 4,
+      subject: "Art & Crafts",
+      location: "Paris",
+      price: 90,
+      spaces: 5,
+      icon: "fas fa-paint-brush",
+      images: "img/4.jpeg"
     },
     {
-        id: 5,
-        subject: "Nutritional Science",
-        location: "Italy",
-        price: 35,
-        spaces: 5,
-        images: "img/5.png"
+      id: 5,
+      subject: "Nutritional Science",
+      location: "Italy",
+      price: 30,
+      spaces: 5,
+      icon: "fab fa-apple",
+      images: "img/5.png"
     },
     {
-        id: 6,
-        subject: "Chemistry",
-        location: "Washington DC",
-        price: 65,
-        spaces: 5,
-        images: "img/6.jpg"
+      id: 6,
+      subject: "Chemistry",
+      location: "Washington DC",
+      price: 65,
+      spaces: 5,
+      icon: "fas fa-atom",
+      images: "img/6.jpg"
     },
     {
-        id: 7,
-        subject: "History",
-        location: "Germany",
-        price: 15,
-        spaces: 5,
-        images: "img/7.png"
+      id: 7,
+      subject: "History",
+      location: "Germany",
+      price: 15,
+      spaces: 5,
+      icon: "fas fa-landmark",
+      images: "img/7.png"
     },
     {
-        id: 8,
-        subject: "Accounting",
-        location: "New York",
-        price: 100,
-        spaces: 5,
-        images: "img/8.jpg"
+      id: 8,
+      subject: "Accounting",
+      location: "New York",
+      price: 100,
+      spaces: 5,
+      icon: "fas fa-money-bill-alt",
+      images: "img/8.jpg"
     },
     {
-        id: 9,
-        subject: "Literature",
-        location: "Australia",
-        price: 90,
-        spaces: 5,
-        images: "img/9.jpg"
+      id: 9,
+      subject: "Literature",
+      location: "Australia",
+      price: 90,
+      spaces: 5,
+      icon: "fas fa-book",
+      images: "img/9.jpg"
     },
     {
-        id: 10,
-        subject: "Biology",
-        location: "Japan",
-        price: 35,
-        spaces: 5,
-        images: "img/10.jpg"
+      id: 10,
+      subject: "French",
+      location: "Japan",
+      price: 35,
+      spaces: 5,
+      icon: "fas fa-assistive-listening-systems",
+      images: "img/10.jpg"
     },
-]
+  ]
